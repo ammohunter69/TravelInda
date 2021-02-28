1c2595708d578ae03f7e4cbe5cf07467f1cd7f03
ammohunter69 <kumarsunilrulz@gmail.com>
2021-02-28 11:00:52 +0530

    server production update

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 // import products from "./data/products.js";
 import connectDB from "./config/db.js";
 // import chalk from "chalk";
@@ -9,11 +10,20 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send(`API is running.`);
-});
-
 app.use("/api/states", stateRoutes);
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send(`API is running.`);
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
